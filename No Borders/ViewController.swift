303573fa5a3acb55c6a249382a46963444957642
JTzBallin515 <jeremy.rodriguez@marquette.edu>
2017-10-08 14:30:27 -0500

    Initial Commit

---
+++ No Borders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  No Borders
+//
+//  Created by Jeremy Rodriguez on 10/8/17.
+//  Copyright © 2017 Jeremy Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
